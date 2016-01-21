9e61a5cd309a9fde9d0e56ea6a8f530152ff739b
Stefano Teodorani <teopost@gmail.com>
2016-01-21 10:05:18 +0100

    Initial commit

---
+++ MoodleSharp/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web; // Agigunta a mano reference a System.Web
+using Newtonsoft.Json;
+
+
+/*
+    Riferimenti:
+
+    https://moodle.org/mod/forum/discuss.php?d=210866
+    http://cipcnet.insa-lyon.fr/Members/ppollet/public/moodlews/
+    http://forums.asp.net/t/1814455.aspx?Trouble+consuming+Moodle+web+services+from+ASP+NET+website
+
+    http://moodle.apexnet.it/webservice/soap/server.php?wsdl=1&wstoken=c9554589d32bd8afbda4ca87119e75ad
+    http://www.azhowto.com/creating-a-moodle-user-via-web-services/
+    http://www.moodletips.com/soap-web-services-wsdl
+    https://delog.wordpress.com/2010/08/31/integrating-a-c-app-with-moodle-using-xml-rpc/
+    https://docs.moodle.org/dev/Web_services
+
+    http://moodle.apexnet.it/login/token.php?username=admin&password=password-999&service=moodle_mobile_app (questo e' un servizio)
+ 
+    https://github.com/zikzakmedia/python-moodle
+ 
+ */
+
+ 
+
+namespace MoodleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String token = "7e708f0bf5b3911ace4e4922d220fbb0x";
+
+            string base_url = @"http://moodle.apexnet.it";
+
+            MoodleUser user = new MoodleUser();
+            user.username = HttpUtility.UrlEncode("mrossi");
+            user.password = HttpUtility.UrlEncode("fakepassword");
+            user.firstname = HttpUtility.UrlEncode("Mario");
+            user.lastname = HttpUtility.UrlEncode("Rossi");
+            user.email = HttpUtility.UrlEncode("mariorossi@wedo-fake.com");
+
+            List<MoodleUser> userList = new List<MoodleUser>();
+            userList.Add(user);
+
+            Array arrUsers = userList.ToArray();
+
+            String postData = String.Format("users[0][username]={0}&users[0][password]={1}&users[0][firstname]={2}&users[0][lastname]={3}&users[0][email]={4}",  user.username, user.password, user.firstname, user.lastname, user.email);
+
+            string createRequest = string.Format(base_url + "/webservice/rest/server.php?wstoken={0}&wsfunction={1}&moodlewsrestformat=json", token, "core_user_create_users");
+
+            // Call Moodle REST Service
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(createRequest);
+            req.Method = "POST";
+            req.ContentType = "application/x-www-form-urlencoded";
+
+            // Encode the parameters as form data:
+            byte[] formData = UTF8Encoding.UTF8.GetBytes(postData);
+            req.ContentLength = formData.Length;
+
+            // Write out the form Data to the request:
+            using (Stream post = req.GetRequestStream())
+            {
+                post.Write(formData, 0, formData.Length);
+            }
+
+
+            // Get the Response
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            Stream resStream = resp.GetResponseStream();
+            StreamReader reader = new StreamReader(resStream);
+            string contents = reader.ReadToEnd();
+
+            // Deserialize
+            JsonSerializer serializer = new JsonSerializer();
+
+            if (contents.Contains("exception"))
+            {
+                // Error
+                MoodleException moodleError = JsonConvert.DeserializeObject<MoodleException>(contents);
+            }
+            else
+            {
+                // Success
+                List<MoodleCreateUserResponse> newUsers = JsonConvert.DeserializeObject<List<MoodleCreateUserResponse>>(contents);
+            }
+            
+        }
+
+        public class MoodleUser
+        {
+            public string username { get; set; }
+            public string password { get; set; }
+            public string firstname { get; set; }
+            public string lastname { get; set; }
+            public string email { get; set; }
+        }
+
+        public class MoodleCreateUserResponse
+        {
+            public string id { get; set; }
+            public string username { get; set; }
+        }
+
+        public class MoodleException
+        {
+            public string exception { get; set; }
+            public string errorcode { get; set; }
+            public string message { get; set; }
+            public string debuginfo { get; set; }
+        }
+
+   
+    }
+}
---
+++ MoodleSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MoodleSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MoodleSharp")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0fa70e5c-1a97-4c88-909e-6d543a74c3bb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
